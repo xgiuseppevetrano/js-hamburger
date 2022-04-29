3969c93df2c33b825833b969f835675ea76da3dc
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-04-29 17:52:42 +0200

    add js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-// 1. Assegnare un id al div.hamburger-menu
+// 1. Prendere gli elementi che ci servono attraverso la classe o l'id
+const hamburgerMenu = document.getElementsByClassName("hamburger-menu")[0];
+const openMenu = document.getElementById ("open-menu");
+const closeMenu = document.getElementById ("close-menu");
+
 // 2. Cliccando div.hamburger-menu passa da display:none a display:block
+openMenu.addEventListener ('click', 
+    function () {
+        hamburgerMenu.style.display = "block";
+    }
+)
+
 // 3. Cliccando la x div.hamburger-menu passa nuovamente a display: none
+closeMenu.addEventListener ('click', 
+    function () {
+        hamburgerMenu.style.display = "none";
+    }
+)
